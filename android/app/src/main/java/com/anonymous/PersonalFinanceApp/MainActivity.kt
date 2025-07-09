347b0a301d8361147761ffea56523b4f15f127b3
Wu Chun Ming <162427371+Wu-Chun-Ming@users.noreply.github.com>
2025-07-09 09:59:00 +0800

    Recompile project

--- android/app/src/main/java/com/anonymous/PersonalFinanceApp/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/PersonalFinanceApp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.PersonalFinanceAnalyticsAndPlanningMobileApp
+package com.anonymous.PersonalFinanceApp
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/anonymous/PersonalFinanceApp/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/PersonalFinanceApp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.PersonalFinanceAnalyticsAndPlanningMobileApp
+package com.anonymous.PersonalFinanceApp
 
 import android.app.Application
 import android.content.res.Configuration
