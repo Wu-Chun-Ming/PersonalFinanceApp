b29f2c285ddfc2d4dd32deb32d42b87c12f6fff2
Wu Chun Ming <162427371+Wu-Chun-Ming@users.noreply.github.com>
2025-07-18 12:28:17 +0800

    Add transations page

---
+++ app/(tabs)/transactions.tsx
@@ -0,0 +1,279 @@
+import { Color, useFont } from '@shopify/react-native-skia';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { Href, router } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { BarGroup, CartesianChart } from 'victory-native';
+
+// Gluestack UI
+import { Button, ButtonText } from '@/components/ui/button';
+import { Fab, FabIcon } from '@/components/ui/fab';
+import { HStack } from '@/components/ui/hstack';
+import { AddIcon } from '@/components/ui/icon';
+import { VStack } from '@/components/ui/vstack';
+
+// Custom import
+import inter from "@/assets/inter-medium.ttf";
+import { TRANSACTION_TYPE_COLORS } from '@/constants/Colors';
+import { EXPENSE_CATEGORIES, INCOME_CATEGORIES, TransactionCategory, TransactionProps, TransactionType } from '@/constants/Types';
+import { fetchTransactions } from '@/db/transactions';
+
+const TransactionScreen = () => {
+    const queryClient = useQueryClient();
+    const font = useFont(inter, 12);
+    const {
+        data: transactions,
+        isLoading,
+        isError,
+        isSuccess,
+        isRefetchError,
+        isRefetching,
+        refetch
+    } = useQuery({
+        queryKey: ['transactions'],
+        queryFn: async () => {
+            try {
+                return await fetchTransactions();
+            } catch (error) {
+                console.error(error);
+                return [];
+            }
+        }
+    });
+    const [expenseTotal, setExpenseTotal] = useState<number>(0);
+    const [incomeTotal, setIncomeTotal] = useState<number>(0);
+
+    // Calculate the total amount based on transaction type and category
+    const getTransactionBreakdownByType = (categories: TransactionCategory[], transactionType: TransactionType) => (
+        categories.map((category) => {
+            const total = (transactions as TransactionProps[]).filter(transaction => transaction.type === transactionType)
+                .filter((t) => t.category === category)
+                .reduce((sum, t) => sum + t.amount, 0);
+
+            return {
+                category,
+                total,
+            };
+        })
+    );
+
+    const TransactionsListing = ({ type }: { type: "expense" | "income" }) => {
+        const transactionType = type === 'expense' ? TransactionType.EXPENSE : TransactionType.INCOME;
+        const categories = type === 'expense' ? EXPENSE_CATEGORIES : INCOME_CATEGORIES;
+
+        return (
+            <VStack>
+                {(getTransactionBreakdownByType(categories, transactionType))
+                    .sort((a, b) => b.total - a.total)  // Sort in descending order by 'amount'
+                    .slice(0, 5)  // Limit to the top 5 category
+                    .map((item, index) => {
+                        if (item.total != 0) {
+                            return (
+                                <HStack
+                                    key={index}
+                                    className='justify-between'
+                                    style={{
+                                        marginHorizontal: 20,
+                                        marginVertical: 10,
+                                    }}
+                                >
+                                    <View style={{
+                                        width: '50%',
+                                    }}>
+                                        <Text style={styles.text}>{item.category}</Text>
+                                    </View>
+
+                                    <Text style={styles.text}>RM</Text>
+                                    <View
+                                        style={{
+                                            width: '30%',
+                                            alignItems: 'flex-end',
+                                        }}
+                                    >
+                                        <Text style={styles.text}>{item.total.toFixed(2)}</Text>
+                                    </View>
+                                </HStack>
+                            );
+                        }
+                    })}
+            </VStack>
+        );
+    };
+
+    const transactionByMonth = (transactions: TransactionProps[]) => {
+        const months_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        const expenseTransactions = transactions.filter(transaction => transaction.type === TransactionType.EXPENSE);
+        const incomeTransactions = transactions.filter(transaction => transaction.type === TransactionType.INCOME);
+
+        const transactionByMonthArray = months_num.map((month) => {
+            const expenseTotalByMonth = expenseTransactions
+                .filter((t) => {
+                    if (!t.date) return false;
+
+                    const transactionMonth = new Date(t.date.toString()).getMonth() + 1;
+                    return transactionMonth === month;
+                })
+                .reduce((sum, t) => sum + t.amount, 0);
+
+            const incomeTotalByMonth = incomeTransactions
+                .filter((t) => {
+                    if (!t.date) return false;
+
+                    const transactionMonth = new Date(t.date.toString()).getMonth() + 1;
+                    return transactionMonth === month;
+                })
+                .reduce((sum, t) => sum + t.amount, 0);
+
+            return {
+                month: month,
+                expensePerMonth: expenseTotalByMonth,
+                incomePerMonth: incomeTotalByMonth,
+            };
+        });
+
+        return transactionByMonthArray;
+    }
+
+    useEffect(() => {
+        // Calculate total for expense and income categories
+        if (transactions) {
+            setExpenseTotal(getTransactionBreakdownByType(EXPENSE_CATEGORIES, TransactionType.EXPENSE).reduce((sum, { total }) => sum + total, 0));
+            setIncomeTotal(getTransactionBreakdownByType(INCOME_CATEGORIES, TransactionType.INCOME).reduce((sum, { total }) => sum + total, 0));
+        }
+    }, [transactions]);
+
+    // If still loading or refetching
+    if (isLoading || isRefetching) {
+        return (
+            <View style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <ActivityIndicator size={80} color="#0000ff" />
+            </View>
+        );
+    }
+    // If error occurs
+    if (isError || isRefetchError) {
+        return (
+            <View style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <Text style={{ color: 'red' }}>Error loading data</Text>
+                <Button onPress={() =>
+                    queryClient.invalidateQueries({ queryKey: ['transactions', transactions] })
+                } >
+                    <ButtonText>Try again</ButtonText>
+                </Button>
+            </View>
+        );
+    }
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            {/* Bar Chart */}
+            <View style={{
+                height: "40%",
+                paddingVertical: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <View style={{
+                    width: '95%',
+                    height: "100%",
+                }}>
+                    {transactions ? <CartesianChart data={
+                        transactionByMonth(transactions as TransactionProps[])
+                    }
+                        xKey="month"
+                        xAxis={{
+                            font,
+                            tickCount: 12,
+                            formatXLabel: (value) => {
+                                const date = new Date(2024, value - 1);
+                                return date.toLocaleString("default", { month: "short" });
+                            },
+                        }}
+                        yKeys={["expensePerMonth", "incomePerMonth"]}
+                        axisOptions={{
+                            font,
+                            lineColor: "#d4d4d8",
+                        }}
+                        domainPadding={{ left: 20, right: 20, top: 30 }}
+                    >
+                        {({ points, chartBounds }) => (
+                            // Bar Group
+                            <BarGroup
+                                chartBounds={chartBounds}
+                                betweenGroupPadding={0.3}
+                                withinGroupPadding={0.1}
+                            >
+                                <BarGroup.Bar points={points.expensePerMonth} color={TRANSACTION_TYPE_COLORS[TransactionType.EXPENSE]} />
+                                <BarGroup.Bar points={points.incomePerMonth} color={TRANSACTION_TYPE_COLORS[TransactionType.INCOME]} />
+                            </BarGroup>
+                        )}
+                    </CartesianChart>
+                        : <View style={{
+                            flex: 1,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                            <Text style={[styles.text, { fontWeight: 'bold' }]}>No transaction data available.</Text>
+                        </View>}
+                </View>
+            </View>
+
+            <ScrollView>
+                <View style={{
+                    margin: 10,
+                }}>
+                    {/* Expense Heading */}
+                    <HStack
+                        className='justify-between'
+                        style={{
+                            padding: 20,
+                            backgroundColor: TRANSACTION_TYPE_COLORS[TransactionType.EXPENSE],
+                            borderRadius: 20,
+                            alignItems: 'center',
+                        }}>
+                        <Text style={[styles.text, {
+                            fontWeight: 'bold',
+                            textDecorationLine: 'underline',
+                        }]}>Top 5 Expense Categories</Text>
+                    </HStack>
+                    {/* Expense Total by Categories */}
+                    {transactions && <TransactionsListing type="expense" />}
+
+                    {/* Income Heading */}
+                    <HStack
+                        className='justify-between'
+                        style={{
+                            padding: 20,
+                            backgroundColor: TRANSACTION_TYPE_COLORS[TransactionType.INCOME],
+                            borderRadius: 20,
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Text style={[styles.text, {
+                            fontWeight: 'bold',
+                            textDecorationLine: 'underline',
+                        }]}>Top 5 Income Categories</Text>
+                    </HStack>
+                    {/* Income Total by Categories */}
+                    {transactions && <TransactionsListing type="income" />}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 15,
+    },
+});
+
+export default TransactionScreen;
